7da3fc2641e67cb2a61068596200909dd536c6ca
atolle - Andrew Tolle <atolle@outlook.com>
2017-08-05 12:49:51 -0500

    Added async user search suggestion functionality.

--- TabRepository/Controllers/FriendsController.cs
+++ TabRepository/Controllers/FriendsController.cs
@@ -1,16 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
+using TabRepository.Data;
 
 namespace TabRespository.Controllers
 {
     public class FriendsController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public FriendsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Friends
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Search(string searchString)
+        {
+            var usernames = _context.Users.Where(u => u.UserName.StartsWith(searchString)).Select(n => n.UserName).ToList();
+            return Json(usernames);
         }
     }
 }
--- TabRepository/wwwroot/js/site.js
+++ TabRepository/wwwroot/js/site.js
@@ -1,1 +1,39 @@
-﻿// Write your Javascript code.
+﻿var searchTimeout = undefined;
+$(document).on('keyup', '.search-form input', function (e) {
+    if (searchTimeout) {
+        clearTimeout(searchTimeout);
+    }
+
+    var thisForm = $(this);
+
+    searchTimeout = setTimeout(function () {
+        $.ajax({
+            type: "POST",
+            url: "/Friends/Search",
+            data: { searchString: thisForm[0].value },
+            success: function (result) {
+                addSearchSuggestions(result);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }, 500);
+});
+
+function addSearchSuggestions(suggestions) {
+    var container = document.getElementsByClassName("search-results");
+    container = container[0];
+
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
+    }
+
+    for (var i = 0; i < suggestions.length; i++) {
+        var input = document.createElement("li");
+        input.className = "list-group-item";
+        input.innerHTML = suggestions[i];
+        input.name = suggestions[i];
+        container.appendChild(input);
+    }
+}

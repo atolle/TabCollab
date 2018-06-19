b3aecd878c504fa3a1765773643508556b9754a5
atolle - Andrew Tolle <atolle@outlook.com>
2018-06-18 20:19:07 -0500

    Added server-side validation for player and download. Enlarged panel images.

--- TabRepository/Controllers/TabFilesController.cs
+++ TabRepository/Controllers/TabFilesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using TabRepository.Data;
 
@@ -21,28 +23,82 @@ namespace TabRepository.Controllers
         }
 
         // GET: TabFile
-        // TODO: Add user validation to this method
-        public FileResult Download(int id)
+        public ActionResult Download(int id)
         {
-            var file = _context.TabFiles.Single(f => f.Id == id);
+            string currentUserId = User.GetUserId();
 
-            if (file == null)
+            try
             {
-                // TO-DO: Handle this scenario
+                var tabVersionInDb = _context.TabVersions.SingleOrDefault(v => v.Id == id && v.UserId == currentUserId);
+
+                // If we are not the owner, are we a contributor?
+                if (tabVersionInDb == null)
+                {
+                    tabVersionInDb = (from tabVersion in _context.TabVersions
+                                      join tab in _context.Tabs on tabVersion.TabId equals tab.Id
+                                      join album in _context.Albums on tab.AlbumId equals album.Id
+                                      join project in _context.Projects on album.ProjectId equals project.Id
+                                      join contributor in _context.ProjectContributors on project.Id equals contributor.ProjectId
+                                      where contributor.UserId == currentUserId
+                                      select tabVersion).Include(u => u.User).FirstOrDefault();
+
+                    if (tabVersionInDb == null)
+                    {
+                        return NotFound();
+                    }
+                }
+
+                var tabFileInDb = _context.TabFiles.Single(f => f.Id == id);
+
+                if (tabFileInDb == null)
+                {
+                    return NotFound();
+                }
+
+                byte[] fileBytes = tabFileInDb.TabData;
+                string fileName = tabFileInDb.Name;
+
+                return File(fileBytes, "application/octet-stream", fileName);
             }
-
-            byte[] fileBytes = file.TabData;
-            string fileName = file.Name;
-
-            return File(fileBytes, "application/octet-stream", fileName);
+            catch
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
         }
 
         // GET: TabFile
-        // TODO: Add user validation to this method
-        public ViewResult Player(int id)
+        public ActionResult Player(int id)
         {
-            ViewBag.Id = id.ToString();
-            return View(); 
+            string currentUserId = User.GetUserId();
+
+            try
+            { 
+                var tabVersionInDb = _context.TabVersions.SingleOrDefault(v => v.Id == id && v.UserId == currentUserId);
+
+                // If we are not the owner, are we a contributor?
+                if (tabVersionInDb == null)
+                {
+                    tabVersionInDb = (from tabVersion in _context.TabVersions
+                                      join tab in _context.Tabs on tabVersion.TabId equals tab.Id
+                                      join album in _context.Albums on tab.AlbumId equals album.Id
+                                      join project in _context.Projects on album.ProjectId equals project.Id
+                                      join contributor in _context.ProjectContributors on project.Id equals contributor.ProjectId
+                                      where contributor.UserId == currentUserId
+                                      select tabVersion).Include(u => u.User).FirstOrDefault();
+
+                    if (tabVersionInDb == null)
+                    {
+                        return NotFound();
+                    }
+                }
+
+                ViewBag.Id = id.ToString();
+                return View();
+            }
+            catch
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }

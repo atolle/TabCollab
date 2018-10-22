437aeaf51ad3540ab0cd21ea859dfc979bc7d487
atolle - Andrew Tolle <atolle@outlook.com>
2018-10-21 21:26:22 -0500

    Initial changes for Dashboard layout on small devices.

--- TabRepository/Controllers/TabVersionsController.cs
+++ TabRepository/Controllers/TabVersionsController.cs
@@ -314,6 +314,76 @@ namespace TabRepository.Controllers
         }
 
         [HttpGet]
+        public ActionResult GetTabVersionsList(int id)
+        {
+            // Return a list of all TabVersions belonging to the current user for current Tab (id)
+            string currentUserId = User.GetUserId();
+
+            try
+            {
+                var tabInDb = _context.Tabs.SingleOrDefault(t => t.Id == id && t.UserId == currentUserId);
+
+                // If we are not the owner, are we a contributor?
+                if (tabInDb == null)
+                {
+                    tabInDb = (from tab in _context.Tabs
+                               join album in _context.Albums on tab.AlbumId equals album.Id
+                               join project in _context.Projects on album.ProjectId equals project.Id
+                               join contributor in _context.ProjectContributors on project.Id equals contributor.ProjectId
+                               where contributor.UserId == currentUserId && tab.Id == id
+                               select tab).FirstOrDefault();
+
+                    if (tabInDb == null)
+                    {
+                        return NotFound();
+                    }
+                }
+
+                var tabVersionsInDb = _context.TabVersions
+                        .Include(v => v.TabFile)
+                        .Include(v => v.User)
+                        .Where(v => v.TabId == id)
+                        .OrderBy(v => v.Version)
+                        .ToList();
+
+                var albumInDb = _context.Tabs.Include(t => t.Album).SingleOrDefault(t => t.Id == id).Album;
+
+                if (tabVersionsInDb == null || tabInDb == null || albumInDb == null)
+                {
+                    return NotFound();
+                }
+
+                // Load Tab Versions into view model to be able to send over whether the current user is the owner of the tab
+                List<TabVersionViewModel> tabVersions = new List<TabVersionViewModel>();
+
+                foreach (var tabVersion in tabVersionsInDb)
+                {
+                    var tabVersionViewModel = new TabVersionViewModel()
+                    {
+                        TabVersion = tabVersion,
+                        IsOwner = tabInDb.UserId == currentUserId || tabVersion.UserId == currentUserId
+                    };
+
+                    tabVersions.Add(tabVersionViewModel);
+                }
+
+                var viewModel = new TabVersionIndexViewModel()
+                {
+                    TabVersions = tabVersions,
+                    TabName = tabInDb.Name,
+                    TabId = id,
+                    AlbumId = albumInDb.Id
+                };
+
+                return PartialView("_TabVersionList", viewModel);
+            }
+            catch
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet]
         public ActionResult GetEmptyTabVersionsTable(int? id)
         {
             if (id == null)

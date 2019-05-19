07bcf165ff5cc4f5f66f43a9795fce211b90a51a
atolle - Andrew Tolle <atolle@outlook.com>
2019-05-18 22:32:23 -0500

    Added Stripe Webhook controller. Part of #97

---
+++ TabRepository/Controllers/Api/StripeWebhookController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Stripe;
+using TabRepository.Data;
+using TabRepository.Helpers;
+
+namespace TabRepository.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/StripeWebhook")]
+    public class StripeWebhookController : Controller
+    {
+        private ApplicationDbContext _context;
+        private IConfiguration _configuration;
+
+        public StripeWebhookController(ApplicationDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        [HttpPost]
+        public IActionResult Index()
+        {
+            var json = new StreamReader(HttpContext.Request.Body).ReadToEnd();
+            var stripeEvent = EventUtility.ParseEvent(json);
+
+            string subscriptionId = "";
+
+            switch (stripeEvent.Type)
+            {
+                case "customer.subscription.updated":
+                    subscriptionId = (stripeEvent.Data.Object as Subscription).Id;
+                    break;
+                case "invoice.payment_succeeded":
+                case "invoice.payment_failed":
+                    subscriptionId = (stripeEvent.Data.Object as Invoice).SubscriptionId;
+                    break;
+            }
+
+            var subscriptionInDb = _context.StripeSubscriptions.Include(s => s.Customer).Where(s => s.Id == subscriptionId).FirstOrDefault();
+
+            if (subscriptionInDb != null)
+            {
+                var userInDb = _context.Users.Where(u => u.Id == subscriptionInDb.Customer.UserId).FirstOrDefault();
+                var subscription = StripeProcessor.GetSubscription(_configuration, subscriptionInDb);
+
+                if (subscription.Status.ToLower() == "active" || subscription.Status.ToLower() == "trialing")
+                {
+                    userInDb.AccountType = Models.AccountViewModels.AccountType.Subscription;
+                    userInDb.SubscriptionExpiration = subscription.CurrentPeriodEnd;
+                }
+                else
+                {
+                    userInDb.AccountType = Models.AccountViewModels.AccountType.Free;
+                }
+
+                _context.SaveChanges();
+            }
+
+            return new StatusCodeResult(StatusCodes.Status200OK);
+        }
+    }
+}

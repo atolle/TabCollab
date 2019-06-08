2929536a3249453382c6b07912852f910726beb8
atolle - Andrew Tolle <atolle@outlook.com>
2019-06-08 14:55:18 -0500

    Improve home page load time. Added bug report. Added install button for Chrome. #109 #110

---
+++ TabRepository/wwwroot/sw.js
@@ -0,0 +1,12 @@
+﻿// Listen for install event, set callback
+self.addEventListener('install', function (event) {
+    // Perform some task
+});
+
+self.addEventListener('activate', function (event) {
+    // Perform some task
+});
+
+self.addEventListener('fetch', function (event) {
+    // Perform some task
+});
